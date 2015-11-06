20eb167b4b8d5570efdea9da08fdd513bd33397c
Mathias Schreck <schreck.mathias@googlemail.com>
2015-11-07 00:21:23 +0100

    Initial commit

---
+++ test/lib/rules/prefer-reject.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var RuleTester = require('eslint').RuleTester,
+    rule = require('../../../lib/rules/prefer-reject.js'),
+    ruleTester = new RuleTester();
+
+ruleTester.run('prefer-reject', rule, {
+    valid: [
+        'R.filter(a);',
+        'filter(a);',
+        'R.complement(a);',
+        'complement(a);',
+        'P.filter(R.complement(a));',
+        'R.find(R.complement(a));',
+        'R.filter(f(a));',
+        'R.filter(P.complement(a));',
+        'R.filter(complement(a));'
+    ],
+
+    invalid: [
+        {
+            code: 'R.filter(R.complement(a));',
+            errors: [
+                {
+                    message: 'R.filter used with negated predicate: Use R.reject instead.',
+                    line: 1,
+                    column: 1,
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'R.filter(R.complement(a), [1,2,3]);',
+            errors: [
+                {
+                    message: 'R.filter used with negated predicate: Use R.reject instead.',
+                    line: 1,
+                    column: 1,
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'R["filter"](R.complement(a));',
+            errors: [
+                {
+                    message: 'R.filter used with negated predicate: Use R.reject instead.',
+                    line: 1,
+                    column: 1,
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'R.filter(R["complement"](a));',
+            errors: [
+                {
+                    message: 'R.filter used with negated predicate: Use R.reject instead.',
+                    type: 'CallExpression'
+                }
+            ]
+        }
+    ]
+});
